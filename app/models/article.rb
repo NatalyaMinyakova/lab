cdc501352305056d64c71f1603c91ac15b8c765d
Natalya <natalya@natalya-VirtualBox.(none)>
2012-11-24 20:51:12 +0400

    add state to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,24 @@
 class Article < ActiveRecord::Base
-  attr_accessible :text, :title
-  
-  validates :title, :presence => true, 
+  attr_accessible :text, :title, :state_event
+
+  validates :title, :presence => true,
 	            :length => { :minimum => 5 }
+
+  state_machine :state, initial: :unpublished do
+    state :published
+    state :unpubslished
+    state :deleted
+
+    event :publish do
+      transition :unpublished => :published
+    end
+
+    event :unpublish do
+      transition :published => :unpublished
+    end
+
+    event :del do
+      transition [:unpublished, :published] => :deleted
+    end
+  end
 end
