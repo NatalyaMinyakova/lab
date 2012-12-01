f939214a4cb99162ec7f93ade221a65d3dc8ba1f
Natalya <natalya@natalya-VirtualBox.(none)>
2012-12-01 20:39:53 +0400

    json in controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,43 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @article}
+    end
   end
 
   def create
     @article = Article.new(params[:article])
-    if @article.save
-      redirect_to :action => :show, :id => @article.id
-    else
-      render 'new'
+
+    respond_to do |format|
+      if @article.save
+        format.html {redirect_to(@article)}
+        format.json {render json: @article, status: :created, location: @article}
+      else
+        format.html {render action: "new"}
+        format.json {render json: @article.errors, status: :unprocessable_entity}
+      end
     end
   end
 
   def show
     @article = Article.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @article}
+    end
   end
 
   def index
     @articles = Article.all
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @articles}
+    end
   end
 
   def edit
@@ -26,10 +46,15 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.update_attributes(params[:article])
-      redirect_to :action => :show, :id => @article.id
-    else
-      render 'edit'
+
+    respond_to do |format|
+      if @article.update_attributes(params[:article])
+        format.html {redirect_to(@article)}
+        format.json {head :no_content}
+      else
+        format.html {render action: "edit"}
+        format.json {render json: @article.errors, status: :unprocessable_entity}
+      end
     end
   end
 
@@ -37,6 +62,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to :action => :index
+    respond_to do |format|
+      format.html {redirect_to articles_path}
+      format.json {head :no_content}
+    end
   end
 end
